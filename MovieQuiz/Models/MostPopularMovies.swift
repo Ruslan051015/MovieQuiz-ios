8855963af4b3972b06b487f2df3a52a786815d17
Ruslan051015 <ruslaniosdev@gmail.com>
2023-04-19 01:46:57 +0400

    добавлен загрузчик фильмов

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -1,11 +1,17 @@
 import Foundation
-struct MostPopularMovies {
+struct MostPopularMovies: Codable {
     let errorMessgae: String
     let items: [MostPopularMovie]
 }
 
-struct MostPopularMovie {
+struct MostPopularMovie: Codable {
     let title: String
     let rating: String
     let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
 }
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovie, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    // MARK: - URL
+    private var mostPopularMoviesURL: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_u7rilnbv") else {
+            preconditionFailure("Не удалость создать URL-ссылку")
+        }
+        return url
+    }
+    func loadMovies(handler: @escaping (Result<MostPopularMovie, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesURL) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovie.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
